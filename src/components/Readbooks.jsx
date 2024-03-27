fdb96235bcc387a4d52c90e845e00b65587575c0
regone7 <regone12345@gmail.com>
2024-03-27 17:15:10 +0600

    readbooks page add

--- src/components/Readbooks.jsx
+++ src/components/Readbooks.jsx
@@ -1,9 +1,52 @@
 
+import { IoLocationOutline } from "react-icons/io5";
+import { GrUserManager } from "react-icons/gr";
+import { FaBook } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
-const Readbooks = () => {
+const Readbooks = ({ bookdata }) => {
     return (
-        <div>
-            
+        <div >
+            <div className="flex flex-col lg:flex-row border-2 border-green-200 p-7 rounded-xl ">
+                <div className="lg:w-[25%] p-5 bg-gray-100 rounded-lg flex justify-center items-center">
+                    <img src={bookdata.image} alt="" />
+                </div>
+                <div className="lg:w-[73%] space-y-4 p-5">
+                    <h1 className="font-bold text-2xl  text-center lg:text-start">{bookdata.bookName}</h1>
+                    <p className="text-center lg:text-start">{bookdata.author}</p>
+                    <div className="flex flex-col lg:flex-row justify-start items-center gap-5">
+                        <h1 className="font-bold">Tag</h1>
+                        <div className="flex justify-start items-center gap-1">
+                            <p className="text-green-500">{bookdata.tags[0]}</p>
+                            <p className="text-green-500">{bookdata.tags[1]}</p>
+                            <p className="text-green-500">{bookdata.tags[2]}</p>
+                        </div>
+                        <div className="flex justify-start items-center gap-1">
+                            <IoLocationOutline />
+                            <p>Year of Publishing: {bookdata.yearOfPublishing}</p>
+                        </div>
+                    </div>
+                    <div className="flex justify-start items-center gap-5">
+                        <div className="flex justify-start items-center gap-1">
+                            <GrUserManager />
+                            <p>Publisher: {bookdata.publisher}</p>
+                        </div>
+                        <div className="flex justify-start items-center gap-1">
+                            <FaBook />
+                            <p>{bookdata.totalPages}</p>
+                        </div>
+                    </div>
+                    <hr />
+                    <div className="flex justify-center flex-col lg:flex-row lg:justify-start items-center lg:items-start gap-3">
+                        <p className="w-52 h-7 flex justify-center items-center bg-blue-100 text-blue-700 rounded-full">Category: {bookdata.author}</p>
+                        <p className="w-52 h-7 flex justify-center items-center bg-green-100 text-yellow-700 rounded-full">Rating: {bookdata.author}</p>
+
+                        <Link to={`/books/${bookdata.bookId}`}><p className="w-40 h-7 flex justify-center items-center bg-green-300 text-purple-700 rounded-full">View Details</p></Link>
+
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Listedbooks.jsx
+++ src/pages/Listedbooks.jsx
@@ -1,27 +1,42 @@
 import { useEffect, useState } from "react";
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+// import { IoLocationOutline } from "react-icons/io5";
+// import { GrUserManager } from "react-icons/gr";
+// import { FaBook } from "react-icons/fa";
+import Readbooks from "../components/Readbooks";
 
 const Listedbooks = () => {
-    const [aplyBook,setAplyBook]=useState([]);
+    const [aplyBook, setAplyBook] = useState([]);
     console.log(aplyBook);
-    useEffect(()=>{
-        const getLocaldatas=JSON.parse(localStorage.getItem("books")) ||[];
+    useEffect(() => {
+        const getLocaldatas = JSON.parse(localStorage.getItem("books")) || [];
         setAplyBook(getLocaldatas);
-    },[])
+    }, [])
     return (
         <div className="container mx-auto my-7">
             <div className='text-3xl font-bold h-24 bg-gray-100 rounded-xl flex justify-center items-center'>Books</div>
             <div className='my-5'>
-                <div role="tablist" className="tabs tabs-lifted">
-                    <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 1" checked />
-                    <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 1</div>
+                <Tabs>
+                    <TabList>
+                        <Tab ><h1 className="font-bold">Read Books</h1></Tab>
+                        <Tab><h1 className="font-bold">Wishlist Books</h1></Tab>
+                    </TabList>
 
-                    <input type="radio" name="my_tabs_2" role="tab" className="tab" aria-label="Tab 2" checked />
-                    <div role="tabpanel" className="tab-content bg-base-100 border-base-300 rounded-box p-6">Tab content 2</div>
+                    <TabPanel>
+                        <div className="grid col-span-1 space-y-3 mt-5">
+                            {
+                                aplyBook.map(bookdata => (<Readbooks key={bookdata.bookId} bookdata={bookdata}></Readbooks>))
+                            }
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <h2>Any content 2</h2>
+                    </TabPanel>
+                </Tabs>
 
-                    
-                </div>
             </div>
+
         </div>
     );
 };
